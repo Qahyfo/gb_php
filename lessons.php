c06c375407ea8ec58c1cd9e2ba85e4f52c7ab910
Qahyfo <qahyfo@gmail.com>
2023-02-02 13:28:17 +0500

    дописал 3 задачу

--- lessons.php
+++ lessons.php
@@ -28,3 +28,33 @@ for ($i = 0; $i < 3; $i++) {
 echo ("
 Выполнение всех задач займёт около: $time
 ");
+
+
+
+while (true) {
+    $num = (int) readline("Введите положительное число ");
+    if ($num > 0) {
+        switch ($num % 8) {
+            case 0:
+            case 2:
+                echo "Указательный";
+                break;
+            case 1:
+                echo "Большой";
+                break;
+            case 3:
+            case 7:
+                echo "Средний";
+                break;
+            case 4:
+            case 6:
+                echo "Безымянный";
+                break;
+            case 5:
+                echo "Мизинец";
+                break;
+        }
+    } else {
+        echo "Введено неверное значение";
+    }
+}
