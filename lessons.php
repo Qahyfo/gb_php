d5d7472387d556363fb7d4846326411633dec3d3
Qahyfo <qahyfo@gmail.com>
2023-02-08 18:55:34 +0500

    lesson3

--- lessons.php
+++ lessons.php
@@ -1,60 +1,26 @@
 <?php
+$arr1 = [];
+$arr2 = [];
+$arrLengh = 15;
 
-
-while (true) {
-    echo "Когда вступил на престол император Николай 2? \n";
-    $question = readline("Выберите один из вариантов ответа: \n
-        30 февраля 1894 \n
-        20 октября 1894 \n
-        1 января 1000
-        ");
-    if ($question == "20 октября 1894") {
-        echo 'Всё верно, поздравляем!';
-        break;
-    } else {
-        echo "Неправильно";
-    }
+for ($i = 0; $i < $arrLengh; $i++) {
+    $arr1[$i] = rand(1, 9);
+    $arr2[$i] = rand(1, 9);
+    $arrRes[$i] = $arr1[$i] * $arr2[$i];
 }
 
+print_r($arrRes);
+
+
+
+$array1 = ['космического', 'бесконечного', 'безудержного'];
+$array2 = ['здоровья', 'воображения', 'терпения'];
+$arrResult = [];
 
 for ($i = 0; $i < 3; $i++) {
-    $quest = readfile("Какая задач предстоит перед вами сегодня?");
-    for ($j = 0; $j < 3; $j++) {
-        $time = readline("Сколько времени займёт эта задача? \n");
-        $time += $time;
-    }
+    $arrResult[] = $array1[array_rand($array1)] . ' ' . $array2[array_rand($array2)];
 }
 
-echo ("
-Выполнение всех задач займёт около: $time
-");
+print_r($arrResult);
 
-
-
-while (true) {
-    $num = (int) readline("Введите положительное число ");
-    if ($num > 0) {
-        switch ($num % 8) {
-            case 0:
-            case 2:
-                echo "Указательный";
-                break;
-            case 1:
-                echo "Большой";
-                break;
-            case 3:
-            case 7:
-                echo "Средний";
-                break;
-            case 4:
-            case 6:
-                echo "Безымянный";
-                break;
-            case 5:
-                echo "Мизинец";
-                break;
-        }
-    } else {
-        echo "Введено неверное значение";
-    }
-}
+echo "Дорогой человек, желаем вам " . implode(',', $arrResult);
